2d69eaa7056694e9c86ea36b91334455b4d635c0
DCorradini <corradinidavid@gmail.com>
2024-01-05 17:10:54 +0700

    Test

--- patterns/banner-hero.php
+++ patterns/banner-hero.php
@@ -6,7 +6,7 @@
  * Viewport width: 1400
  */
 ?>
-
+Here it is
 <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained","contentSize":"","wideSize":""}} -->
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)">
 
